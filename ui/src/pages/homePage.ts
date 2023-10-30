50f10732d06c462f5452720410acdb561f8f9242
w.cz <w.chizhenkov@yandex.ru>
2023-10-30 17:58:26 +0400

    added ui base

--- ui/src/pages/homePage.ts
+++ ui/src/pages/homePage.ts
@@ -2,7 +2,6 @@ import { Container } from '@Core/container';
 
 export class HomePage extends Container {
     protected LOCATORS = {
-        featuredIn: this.page.locator('//section[contains(., "As featured in.")]'),
     };
 
     public async open(): Promise<void> {
