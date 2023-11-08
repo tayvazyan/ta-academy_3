e2fd3d60688435a52d2ab2743bff4994cf51751d
Tatevik <tayvazyan20@gmail.com>
2023-11-09 03:47:45 +0400

    Do homework 7

--- integration/__specs__/modules/components/cartPage/ModalAddItem/ModalAddItem.ts
+++ integration/__specs__/modules/components/cartPage/ModalAddItem/ModalAddItem.ts
@@ -1,5 +1,27 @@
 import { Component } from '@Core/component';
+import { fireEvent } from '@testing-library/react';
 
 export class ModalAddItem extends Component {
-    protected selectors = {};
+    protected selectors = {
+        modalInputs: (text: string) => `.//input[@name="${text}"]`,
+        buttonCreate: './/button[text()="Create"]',
+        modalTitle: './/div[@data-testid="modal"]//h2',
+    };
+
+    public async getModalTitle(): Promise<string> {
+        const [title] = await document.waitForXpath(this.selectors.modalTitle);
+        return title.textContent;
+    }
+
+    public async typeText(modalInput: string, text: string): Promise<void> {
+        const [modalInputElement] = await document.waitForXpath(this.selectors.modalInputs(modalInput));
+        fireEvent.change(modalInputElement,  {
+            target: { value: text }
+          });
+    }
+
+    public async clickCreateButton(): Promise<void> {
+        const [button] = await document.waitForXpath(this.selectors.buttonCreate);
+        fireEvent.click(button);
+    }
 }
